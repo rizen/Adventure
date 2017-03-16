7f6e3967e5e5c84b0319d4e8a11022f43d935731
jimeUWOshkosh <mr_osh_vegas@yahoo.com>
2017-03-16 11:04:25 -0500

    Create a import base module

--- perl5/lib/Adventure.pm
+++ perl5/lib/Adventure.pm
@@ -2,4 +2,33 @@ use strict;
 use warnings;
 package Adventure;
 
+=head1 NAME
+
+Adventure - Standardize what Perl configuration Adventure uses.
+
+
+=head1 SYNOPSIS
+
+ use Adventure;
+
+=cut
+
+use 5.010_000;
+
+use strict;
+use warnings;
+
+use mro     ();
+use feature ();
+use utf8    ();
+
+sub import {
+    warnings->import();
+    warnings->unimport( qw/uninitialized/ );
+    strict->import();
+    feature->import( ':5.10' );
+    mro::set_mro( scalar caller(), 'c3' );
+    utf8->import();
+}
+
 1;
