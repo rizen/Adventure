3a193657dd2172ff527abdc810a2110d86fd0be7
Jesse Thompson <jesse.thompson@wisc.edu>
2017-07-05 22:10:44 -0500

    simple docker run instructions

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@ cd perl5
 docker build -t adventure-base .
 cd ..
 docker build -t adventure-mission .
+echo "Assuming everything built OK..."
+echo "run this:"
+echo "docker run -it adventure-mission"
